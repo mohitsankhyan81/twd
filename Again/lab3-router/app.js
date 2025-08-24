fd44945302d27536c930d37a0a84c57812146180
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-24 17:55:28 +0530

    commit

---
+++ Again/lab3-router/app.js
@@ -0,0 +1,23 @@
+const express=require('express');
+
+const app=express();
+
+app.use((req,res,next)=>{
+  console.log('My name is mohit sankhyan');
+  next();
+});
+
+app.use("/common",(req,res)=>{
+  console.log("this is the first page");
+  res.send("This is the firsy page");
+});
+
+app.use("/",(req,res)=>{
+  console.log("this is the second page");
+  res.send("this is the second page");
+})
+
+const port=4664;
+app.listen(port,(req,res)=>{
+  console.log(`http://localhost:${port}`);
+})
--- Again/lab3/app.js
+++ Again/lab3/app.js
@@ -1,13 +1,20 @@
-const express=require('express');
+const exprss=require('express');
 
-const app=express();
+const app=exprss();
 
-app.use('/',(req,res)=>{
-  console.log("app.is working");
+app.use((req,res,next)=>{
+  console.log('welcome to out code',req.url);
+  res.send('My name is mohit sankhyan');
+  next();
 });
 
-const port=5436;
+app.use((req,res,next)=>{
+  console.log("here i say hello world",req.url);
+  res.send("<h1>My name is your name</h1>");
+  next();
+})
 
-app.listen(port,()=>{
-  console.log(`http://localhost:${port}`);
+
+app.listen(4534,()=>{
+  console.log(`http://localhost:4534`);
 })
