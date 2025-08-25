76e97e41a4938ebd509f593d46884491af0c06c1
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-25 11:38:12 +0530

    commit

--- Again/lab3-router/app.js
+++ Again/lab3-router/app.js
@@ -3,21 +3,23 @@ const express=require('express');
 const app=express();
 
 app.use((req,res,next)=>{
-  console.log('My name is mohit sankhyan');
+  console.log("this is my first vlog",req.url);
   next();
-});
-
-app.use("/common",(req,res)=>{
-  console.log("this is the first page");
-  res.send("This is the firsy page");
-});
-
-app.use("/",(req,res)=>{
-  console.log("this is the second page");
-  res.send("this is the second page");
 })
 
-const port=4664;
+
+app.use("/common",(req,res,next)=>{
+  console.log("this is my name bock :- My name is mohit sankhyan");
+  res.send("<h1>This is the first block of my name</h1>");
+});
+
+app.use((req,res)=>{
+  console.log("this is my second block of my name is mohit sankhyan");
+  res.send("<h1>This the second block of my name and my name is mohit sankhyan</h1>")
+});
+
+const port=4566;
+
 app.listen(port,()=>{
   console.log(`http://localhost:${port}`);
-})
+});
