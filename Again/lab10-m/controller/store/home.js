5f82a2210a33eeda9355823e89a41c7a8a8d9109
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-10 12:39:32 +0530

    commit

--- Again/lab10-m/controller/store/home.js
+++ Again/lab10-m/controller/store/home.js
@@ -6,4 +6,22 @@ const homepage=(req,res,next)=>{
   });
 }
 
+const favlist=(req,res,next)=>{
+  Data.fetchdata(registerHouse=>{
+  res.render('store/fav-list',{registerHouse:registerHouse,pagetitle:'this is the home page'});
+  });
+}
+
+const booking=(req,res,next)=>{
+  res.render('store/booking');
+}
+
+const indexvalue=(req,res,next)=>{
+  Data.fetchdata(registerHouse=>{
+  res.render('store/index',{registerHouse:registerHouse,pagetitle:'this is the home page'});
+  });
+}
+exports.indexvalue=indexvalue;
 exports.homepage=homepage;
+exports.favlist=favlist;
+exports.booking=booking;
--- Again/lab10-m/router/userRouter.js
+++ Again/lab10-m/router/userRouter.js
@@ -2,8 +2,10 @@ const express=require('express');
 
 const userRouter=express.Router();
 
-const { homepage } = require('../controller/store/home');
+const { homepage, favlist, booking, indexvalue } = require('../controller/store/home');
 
 userRouter.get("/",homepage);
-
+userRouter.get('/fav-list',favlist);
+userRouter.get('/booking',booking)
+userRouter.get('/homes',indexvalue);
 module.exports=userRouter;
